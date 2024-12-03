d5c69fc6f9e783e4392b620524ca1737cfdc89b1
Victor Rudkovsky <vpcomtek@gmail.com>
2024-12-03 22:13:58 +0200

    Solution

--- task.sh
+++ task.sh
@@ -1,1 +1,10 @@
 #! /bin/bash
+
+USER=$DB_USER
+PASSWORD=$DB_PASSWORD
+
+mysqldump -u"$USER" -p"$PASSWORD" ShopDB --no-create-db --result-file=shopdb_full_backup.sql
+mysql -u "$USER" -p"$PASSWORD" ShopDBReserve < shopdb_full_backup.sql
+
+mysqldump -u"$USER" -p"$PASSWORD" --no-create-db --no-create-info ShopDB --result-file=shopdb_data_backup.sql
+mysql -u "$USER" -p"$PASSWORD" ShopDBDevelopment < shopdb_data_backup.sql
